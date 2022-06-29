9544f2f37085dea5d9d15abca5dbb062e546cbf9
EnzoWu <enwu2014@hotmail.com>
2022-06-29 13:03:21 -0300

    estrelas update

--- src/components/Estrelas/index.jsx
+++ src/components/Estrelas/index.jsx
@@ -4,17 +4,24 @@ import { faStar } from "@fortawesome/free-solid-svg-icons";
 const Estrelas = ({rate}) => {
     return(
         <StarsArea>
-            <Star 
-            fill={(Math.round(rate) >= 2) ? true : undefined}
-            ><FontAwesomeIcon icon={faStar}/></Star>
-            <Star
-            fill={(Math.round(rate) >= 4) ? true : undefined}><FontAwesomeIcon icon={faStar}/></Star>
-            <Star
-            fill={(Math.round(rate) >= 6) ? true : undefined}><FontAwesomeIcon icon={faStar}/></Star>
-            <Star
-            fill={(Math.round(rate) >= 8) ? true : undefined}><FontAwesomeIcon icon={faStar}/></Star>
-            <Star
-            fill={(Math.round(rate) >= 10) ? true : undefined}><FontAwesomeIcon icon={faStar}/></Star>
+            {rate > 0 ?
+            <>
+                <Star 
+                fill={(Math.round(rate) >= 2) ? true : undefined}
+                ><FontAwesomeIcon icon={faStar}/></Star>
+                <Star
+                fill={(Math.round(rate) >= 4) ? true : undefined}><FontAwesomeIcon icon={faStar}/></Star>
+                <Star
+                fill={(Math.round(rate) >= 6) ? true : undefined}><FontAwesomeIcon icon={faStar}/></Star>
+                <Star
+                fill={(Math.round(rate) >= 8) ? true : undefined}><FontAwesomeIcon icon={faStar}/></Star>
+                <Star
+                fill={(Math.round(rate) >= 10) ? true : undefined}><FontAwesomeIcon icon={faStar}/></Star>
+            </>:
+            <p>Não disponível</p>
+            
+            }
+            
         </StarsArea>
     )
 }
